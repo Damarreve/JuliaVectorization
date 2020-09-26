e48bfbd98cc43f4faa614ef9c0150c99cb52c60a
Danil Filippov <damarreve@list.ru>
2020-09-26 15:36:58 +0400

    Инициализация

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+julia --version >/dev/null 2>&1
+
+[[ $? -ne 0 ]] && echo -e "Приложение \033[0;34mJulia\033[0m \033[1mне найдено\033[0m!" && exit 127
+
+pushd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1
+for File in $(find ./src -type f -name *.jl)
+do
+    echo -e "\033[0;32m"$(basename ${File})"\033[0m:"
+    julia ${File}
+    echo
+done
+popd >/dev/null 2>&1
---
+++ src/Main.jl
@@ -0,0 +1,7 @@
+function printsum(a)
+    # print sum
+    println(summary(a), ": ", repr(a))
+end
+
+a1 = [1, 2, 3]
+printsum(a1)
