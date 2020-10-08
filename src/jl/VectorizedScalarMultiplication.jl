5bbad3e8d83065245e23eadbd0875d07ca90dd97
Danil Filippov <damarreve@list.ru>
2020-10-09 02:45:12 +0400

    Пример векторизации

---
+++ src/jl/VectorizedScalarMultiplication.jl
@@ -0,0 +1,32 @@
+using Dates
+
+size = 500
+ratio = 100
+debug = false
+println("Dimension is ", size)
+
+_start = now()
+result = zeros(size)
+vector = rand(size) * ratio
+matrix = rand(size, size) * ratio
+_end = now()
+println("Random matrix and vector created for ", _end - _start)
+
+if debug
+  display(vector)
+  println()
+  display(matrix)
+  println()
+  println("-----------------")
+end
+
+_start = now()
+result .+= matrix * vector
+_end = now()
+
+if debug
+  display(result)
+  println()
+end
+
+println("Vectorized scalar multiplication took ", _end - _start)
